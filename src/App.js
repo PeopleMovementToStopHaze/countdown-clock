ebba25af1e35c9c4650616dc9e4dda6a0d7cbb91
Groupystinks <jason0911520@gmail.com>
2015-12-12 21:02:26 +0800

    removed react-router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import React, {Component} from 'react';
 import CountdownTimer from './CountdownTimer/CountdownTimer';
-import MobileDetect from 'mobile-detect';
 import Colors from 'utils/ColorCollection';
-
-const styles = require('./App.css');
 
 export default class App extends Component {
   constructor() {
     super();
   }
   render() {
-    const deviceDetect = new MobileDetect(window.navigator.userAgent);
     return (
-      <div>
-        <section>
-          <div className={styles.root}></div>
-          <div className={styles.content_container}>
-              <div className={styles.header_container}>
-                <h3 className={styles.header}>Time Left</h3>
-                <h4 className={styles.header_sm}>to Haze-Free ASEAN</h4>
-              </div>
-          </div>
-          {!deviceDetect.mobile() ? (
-          <CountdownTimer
-            deadline={'Oct 29 2020 00:00:50 UTC+0800'}
-            options={{format: 'year-day-hour-minute-second'}}
-            size="180px"
-            fontSize={32}
-            colorFinished={Colors.white}
-            colorGoing={Colors.grey800}
-            textColor={Colors.grey800} />
-          ) : null}
-        </section>
-      </div>
+        <CountdownTimer
+          deadline={'Oct 29 2020 00:00:50 UTC+0800'}
+          options={{format: 'year-day-hour-minute-second'}}
+          size="180px"
+          fontSize={32}
+          colorFinished={Colors.white}
+          colorGoing={Colors.grey800}
+          textColor={Colors.grey800} />
     );
   }
 }
--- src/client.js
+++ src/client.js
@@ -1,20 +1,5 @@
 import React from 'react';
-import {Router, Route, IndexRoute} from 'react-router';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createHistory, useBasename } from 'history';
 
-const history = useBasename(createHistory)({
-  basename: '/countdown-clock',
-});
-
-const Routes = (
-  <Router history={history}>
-    <Route path="/" component={App}>
-      <IndexRoute component={App} />
-    </Route>
-  </Router>
-);
-
-
-ReactDOM.render(Routes, document.getElementById('CountdownRoot'));
+ReactDOM.render(React.createElement(App), document.getElementById('CountdownRoot'));
